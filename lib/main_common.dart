73e32fe843e8361ed2776b384ae853d5fa0c12a5
Hanna Trapachka <hanna.trapachka@innowise.com>
2024-11-22 11:50:40 +0100

    initial commit

---
+++ lib/main_common.dart
@@ -0,0 +1,62 @@
+import 'package:core/core.dart';
+import 'package:core_ui/core_ui.dart';
+import 'package:data/data.dart';
+import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:navigation/navigation.dart';
+
+import 'error_handler/provider/app_error_handler_provider.dart';
+  
+
+Future<void> mainCommon(Flavor flavor) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  
+  _setupDI(flavor);
+
+  runApp(const App());
+}
+
+void _setupDI(Flavor flavor) {
+  appLocator.pushNewScope(
+    scopeName: unauthScope,
+    init: (_) {
+      AppDI.initDependencies(appLocator, flavor);
+      DataDI.initDependencies(appLocator);
+      DomainDI.initDependencies(appLocator);
+      NavigationDI.initDependencies(appLocator);
+    },
+  );
+}
+  
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final AppRouter appRouter = appLocator<AppRouter>();
+
+    return EasyLocalization(
+      path: AppLocalization.langFolderPath,
+      supportedLocales: AppLocalization.supportedLocales,
+      fallbackLocale: AppLocalization.fallbackLocale,
+      child: Builder(
+        builder: (BuildContext context) {
+          return AppErrorHandlerProvider(
+            child: MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              routerConfig: appRouter.config(),
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+              theme: lightTheme,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+  
+  
---
+++ lib/main_prod.dart
@@ -0,0 +1,8 @@
+import 'package:core/core.dart';
+
+import 'main_common.dart';
+
+void main() {
+  mainCommon(Flavor.prod);
+}
+    
