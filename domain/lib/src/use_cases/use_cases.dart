8518d22e62f9daf7c03c5ea6b18c1f5fb3ee1013
Hanna Trapachka <hanna.trapachka@innowise.com>
2024-12-10 12:59:14 +0100

    fix export

--- domain/lib/src/use_cases/use_cases.dart
+++ domain/lib/src/use_cases/use_cases.dart
@@ -2,5 +2,5 @@ export 'character/get_character_details_use_case.dart';
 export 'character/get_character_list_use_case.dart';
 export 'connectivity/get_connection_status_use_case.dart';
 export 'theme/change_theme_mode_use_case.dart';
-export 'theme/get_theme_mode_use_case.dart';
+export 'theme/listen_theme_mode_changes_use_case.dart';
 export 'use_case.dart';
